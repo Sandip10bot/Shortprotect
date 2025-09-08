bfa47e09ae370b324d4c91c6a7229fb14e5cf123
Sαɳԃιρ <102131200+Sandip10bot@users.noreply.github.com>
2025-09-08 09:26:15 +0530

    Update and rename config.js to vercel.json

--- config.js
+++
@@ -1,3 +0,0 @@
-export const SOFTURL_API_KEY = "27a4d762fdd98dc6ecb8917a88b44c98b7395cde"; // Get from Softurl.in
-export const DOMAIN = "mythobot.vercel.app";          // Your Vercel domain
-export const TOKEN_EXPIRY = 10 * 60 * 1000;           // 10 minutes (optional)
