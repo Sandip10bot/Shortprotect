a9ae1df49648b758b2d614457ce43e84f510d4ed
Sαɳԃιρ <102131200+Sandip10bot@users.noreply.github.com>
2025-09-20 12:28:56 +0530

    Update getUrl.js

--- pages/api/getUrl.js
+++ pages/api/getUrl.js
@@ -6,11 +6,10 @@ export default async function handler(req, res) {
 
   const client = await clientPromise;
   const db = client.db("shortener");
+
   const link = await db.collection("links").findOne({ slug });
 
-  if (link) {
-    res.status(200).json({ url: link.url });
-  } else {
-    res.status(404).json({ error: "Not found" });
-  }
+  if (!link) return res.status(404).json({ error: "Not found" });
+
+  res.status(200).json({ url: link.url });
 }
